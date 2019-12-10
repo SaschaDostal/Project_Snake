bd590b4f9868860418cb67e043bcf1bd8cc3da98
Felix Koerner <91kofe1bif@hft-stuttgart.de>
2019-12-10 08:25:47 +0100

    finally got the css working, was missing an app.use

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.set("view engine", "ejs");
+app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
