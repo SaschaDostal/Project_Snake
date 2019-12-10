7548e295dd9ecea018ba5192e4eb5cd5acf5e017
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2019-12-10 13:35:43 +0100

    show gamecanvas

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
 });
