8bdb68d46894ca7d54a14e5069f1f829ac0d1005
Alexander Hoetzel <91hoal1bif@hft-stuttgart.de>
2019-12-10 17:22:43 +0100

    fix opacity/ add picture

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use("/public", express.static(process.cwd() + "/public"));
-app.use("/public", express.static(process.cwd() + "/views"));
+app.use("/views", express.static(process.cwd() + "/views"));
 
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
